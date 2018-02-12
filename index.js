9d76122f37f1dc11525abfda9e852640512a3de3
Morgan V Hirsch <morganhirsch93@hotmail.co.uk>
2018-02-12 12:29:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var recipes = {[prop]: 1;
+var recipes = {prop: 1;
 
 function updateObjectWithKeyAndValue(object, key, value){
-  var newRecipes = Object.assign({}, recipes, {[key]: value})
+  var newRecipes = Object.assign({}, recipes, {key: value})
   return newRecipes
 }
