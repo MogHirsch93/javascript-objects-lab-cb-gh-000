22c28ed7cabf212ab11f8cac976371d9829a2e72
Morgan V Hirsch <morganhirsch93@hotmail.co.uk>
2018-02-12 12:51:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
- var newRecipe = Object.assign({}, recipes, {[key]: value})
+ var newRecipe = Object.assign({}, recipes, {[key]: value});
  return newRecipe;
 }
 
